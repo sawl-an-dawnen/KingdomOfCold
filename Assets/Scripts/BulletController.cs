db5e1de9b6206b808fb6112aa405a0547105ee07
Ilan Lipsky <102115093+CoinismRS@users.noreply.github.com>
2024-03-02 10:59:54 -0600

    Updated Bullet Spawner
    - Added enum for Bullet Spawn

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    public enum Type { Damage, Time }
+
+    public Type type;
 
     private Transform playerTarget;
     public float speed;
@@ -17,11 +20,23 @@ public class BulletController : MonoBehaviour
         GetComponent<Rigidbody2D>().velocity = direction * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
-        
-        
+        if (other.CompareTag("Player"))
+        {
+            //if player is hit by a DamageBullet, decrement player health.
+            if (isBulletDamage == true)
+            {
+                //TODO: deduct health from player, slow their movement speed = 0.5s, destroy gameObject
+
+            }
+
+
+            //if player is hit by a TimeBullet, increment player remaining time
+            if (isBulletTime == true)
+            {
+                //TODO: increment player's remaining time, slow movement speed = 0.5s, destroy gameObject
+            }
+        }
     }
 }
--- Assets/Scripts/BulletSpawn.cs
+++ Assets/Scripts/BulletSpawn.cs
@@ -6,8 +6,15 @@ using UnityEngine.UIElements;
 
 public class BulletSpawn : MonoBehaviour
 {
+
+    public enum Type { Damage, Time }
+
+    public Type type;
+
     [SerializeField] public GameObject _spawnBulletDamage; //pawn instance
     [SerializeField] public GameObject _spawnBulletTime; //pawn instance
+
+
     public float spawnRate = 5.0f; // rate at which pawns spawns
 
     private float currSpawnTimer = 1.0f;
@@ -26,11 +33,11 @@ public class BulletSpawn : MonoBehaviour
         if (currSpawnTimer <= 0f)
         {
             int bulletType = Random.Range(0, 1);
-            if (bulletType == 0)
+            if (type == Type.Damage)
             {
                 Instantiate(_spawnBulletDamage, gameObject.transform.position, this.transform.rotation, gameObject.transform);
             }
-            if (bulletType == 1)
+            if (type == Type.Time)
             {
                 Instantiate(_spawnBulletTime, gameObject.transform.position, this.transform.rotation, gameObject.transform);
             }
