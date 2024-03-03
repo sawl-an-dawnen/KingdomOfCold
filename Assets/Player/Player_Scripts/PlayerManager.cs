89514cd76fb9ca2df8fa29c0c38c00ecd1aa67ce
Ilan Lipsky <102115093+CoinismRS@users.noreply.github.com>
2024-03-03 00:31:05 -0600

    Added Invincibility Feature
    - Updated PlayerManager.cs script with Invincibility functionality.

--- Assets/Player/Player_Scripts/PlayerManager.cs
+++ Assets/Player/Player_Scripts/PlayerManager.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public float moveSpeed = 10.0f;
+    [Header("Invincibility Settings")]
+    [SerializeField] float invincibleCounter = 2.0f; // Length of Invincibility
+    [SerializeField] float damageCooldown = 2.0f; // Duration of damageCooldown
+
     [HideInInspector]
     public Vector2 moveVector;
 
+    private bool isDamaged;
+    //private bool canHurt = true;
+    private float defaultSpeed;
+    private float timer = 0f;
     private Rigidbody2D rb;
+    private playerMovement movement;
+    private BoxCollider2D boxCollider;
+    private SpriteRenderer spriteRender;
 
     public bool debugMode = false;
     public int playerHealth = 3;
     public int overDriveShieldBurts = 3;
-
     //public float speedPenalty = 30f;
     public float penaltyDuration = 2f;
+    public float moveSpeed = 10.0f;
 
-    private float defaultSpeed;
-    private playerMovement movement;
-    private float timer = 0f;
+
 
     void Start() 
     {
         movement = gameObject.GetComponent<playerMovement>();
         defaultSpeed = movement.moveSpeed;
+        boxCollider = GetComponent<BoxCollider2D>();
+        spriteRender = GetComponent<SpriteRenderer>();
     }
 
     void Update() 
@@ -39,9 +49,23 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    public void TakeDamage()
+    /*public void Invincibility()
     {
-        
+        Debug.Log("Player is invincible.");
+        // If (Player is not invincible)
+        if (!isDamaged && canHurt)
+        {
+            isDamaged = true;
+            StartCoroutine(InitiateInvincibility());
+            canHurt = false; // Disable damage until cooldown is over
+            StartCoroutine(DamageCooldown()); // Start the damage cooldown timer
+            StartCoroutine(InvincibleCounter());
+        }
+    }
+    */
+
+    public void TakeDamage()
+    {        
         playerHealth -= 1;
        
         if (playerHealth <= 0)
@@ -51,6 +75,54 @@ public class PlayerManager : MonoBehaviour
                 Die();
             }
         }
+
+     // Start Invincibility frames
+        Debug.Log("Player is invincible.");
+        //Invincibility();
+        boxCollider.enabled = false;
+        spriteRender.color = new Color(0.0f,0.0f, 0.0f, 0.5f);
+        StartCoroutine(InvincibleCounter());
+
+
+    /*
+        // If (Player is not invincible)
+        if (!isDamaged && canHurt)
+        {
+            isDamaged = true;
+            StartCoroutine(InitiateInvincibility());
+            canHurt = false; // Disable damage until cooldown is over
+            StartCoroutine(DamageCooldown()); // Start the damage cooldown timer
+            StartCoroutine(InvincibleCounter());
+        }
+    */
+
+    }
+
+    /*private IEnumerator InitiateInvincibility()
+    {
+        // Disable Player BoxCollider
+        boxCollider.enabled = false;
+
+        while (!canHurt)
+        {
+            yield return null;
+        }
+    }*/
+
+    /*private IEnumerator DamageCooldown()
+    {
+        yield return new WaitForSeconds(damageCooldown);
+        canHurt = true; // Enable damage after cooldown is over
+    }
+    */
+
+    private IEnumerator InvincibleCounter()
+    {
+        // invincibilityAudio.Play();
+        yield return new WaitForSeconds(invincibleCounter);
+        spriteRender.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        boxCollider.enabled = true;
+        
     }
 
     public void GetSlowed(float speedPenalty)
